bf27afb25b8e87d4bece42a5f7ebd5fca30edc36
basheerdve <bashirnabeel624@gmail.com>
2026-05-20 16:52:27 +0500

    Update badge.tsx

--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -1,6 +1,6 @@
 import { cva, type VariantProps } from "class-variance-authority"
 import * as React from "react"
-import { cn } from "@/lib/utils"
+import { cn } from "./lib/utils"
 
 const badgeVariants = cva(
   "inline-flex h-5 w-fit shrink-0 items-center justify-center gap-1 overflow-hidden rounded-full border border-transparent px-2 py-0.5 text-xs font-medium whitespace-nowrap transition-all",
